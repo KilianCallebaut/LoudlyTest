a4ad0539382b3bc357386928fa2d6dfaa451f342
KilianCallebaut <kilian.callebaut@gmail.com>
2022-05-03 13:35:07 +0200

    Connected adapter to api call

--- app/src/main/java/com/example/loudlytest/MainActivity.kt
+++ app/src/main/java/com/example/loudlytest/MainActivity.kt
@@ -4,24 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
+import com.example.loudlytest.adapters.RepoAdapter
 import com.example.loudlytest.databinding.ActivityMainBinding
 import com.example.loudlytest.requests.RepoRequestQueue
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var count = 0
+    private val repoAdapter = RepoAdapter(mutableListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.SearchButton.setOnClickListener {
-            createRequest(binding.searchField)
+            createRequest(binding.apiContent)
         }
+
+        binding.apiContent.adapter = repoAdapter
+        binding.apiContent.layoutManager = LinearLayoutManager(this)
     }
 
     private fun createRequest(view: View) {
@@ -31,11 +37,13 @@ class MainActivity : AppCompatActivity() {
             Response.Listener { response ->
                 val jsonResponse = response.getJSONArray("items")
                 Toast.makeText(this.applicationContext, jsonResponse.length().toString(), Toast.LENGTH_LONG).show()
+                repoAdapter.add_repos_last(jsonResponse)
             },
             Response.ErrorListener {error ->
                 Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show()
             }
         )
         RepoRequestQueue.getInstance(this).addToRequestQueue(request)
+
     }
 }
--- app/src/main/java/com/example/loudlytest/adapters/RepoAdapter.kt
+++ app/src/main/java/com/example/loudlytest/adapters/RepoAdapter.kt
@@ -1,12 +1,20 @@
 package com.example.loudlytest.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.loudlytest.R
 import com.example.loudlytest.objects.RepoItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.repo_item.view.*
+import org.json.JSONArray
 
-class RepoAdapter (private val repoItems: MutableList<RepoItem>) : RecyclerView.Adapter<RepoViewHolder>() {
+class RepoAdapter(private val repoItems: MutableList<RepoItem>) :
+    RecyclerView.Adapter<RepoAdapter.RepoViewHolder>() {
+
+    class RepoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
         return RepoViewHolder(
             LayoutInflater.from(parent.context).inflate(
@@ -18,11 +26,36 @@ class RepoAdapter (private val repoItems: MutableList<RepoItem>) : RecyclerView.
     }
 
     override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val curRepo = repoItems[position]
+        holder.itemView.apply {
+            repoName.text = curRepo.repoName
+            loginOwner.text = curRepo.loginOwner
+            size.text = curRepo.size
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return repoItems.size
+    }
+
+    fun add_repos(index: Int, data: List<RepoItem>) {
+        repoItems.addAll(index, data)
+        notifyItemRangeChanged(index, data.size)
+    }
+
+    fun add_repos_last(data: JSONArray) {
+        val index = repoItems.size
+        for (i in 0 until data.length()) {
+            val repoObject = data.getJSONObject(i)
+            repoItems.add(
+                RepoItem(
+                    repoObject.getString("name"),
+                    repoObject.getJSONObject("owner").getString("login"),
+                    repoObject.getInt("size").toString()
+                )
+            )
+        }
+        notifyItemRangeChanged(index, data.length())
     }
 
 }
