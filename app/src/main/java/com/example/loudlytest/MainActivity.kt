1bf26e7f75b967943e05083aaff5a2d9a24cbce0
KilianCallebaut <kilian.callebaut@gmail.com>
2022-05-03 12:37:01 +0200

    Created API Call functions

--- app/src/main/java/com/example/loudlytest/MainActivity.kt
+++ app/src/main/java/com/example/loudlytest/MainActivity.kt
@@ -3,20 +3,39 @@ package com.example.loudlytest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.example.loudlytest.databinding.ActivityMainBinding
 import com.example.loudlytest.requests.RepoRequestQueue
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.SearchButton.setOnClickListener {
+            createRequest(binding.searchField)
+        }
     }
 
-    private fun createRequestQueue(view: View) {
-        val queue = RepoRequestQueue.getInstance(this.applicationContext).requestQueue
-        
+    private fun createRequest(view: View) {
+        val url = "https://api.github.com/search/repositories?q=tetris&per_page=10"
+        val request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                val jsonResponse = response.getJSONArray("items")
+                Toast.makeText(this.applicationContext, jsonResponse.length().toString(), Toast.LENGTH_LONG).show()
+            },
+            Response.ErrorListener {error ->
+                Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show()
+            }
+        )
+        RepoRequestQueue.getInstance(this).addToRequestQueue(request)
     }
 }
