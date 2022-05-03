e092cef39cabbe890c75647aec924e7c1cfd943e
KilianCallebaut <kilian.callebaut@gmail.com>
2022-05-03 11:34:39 +0200

    created request queue

--- app/src/main/java/com/example/loudlytest/MainActivity.kt
+++ app/src/main/java/com/example/loudlytest/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.loudlytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.loudlytest.requests.RepoRequestQueue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun createRequestQueue(view: View) {
+        val queue = RepoRequestQueue.getInstance(this.applicationContext).requestQueue
+        
     }
 }
